375b24a67edfe795057185df5109f7c78a84fe81
MiguelRizzi <miguel-rizzi@hotmail.com.ar>
2023-04-18 23:10:16 -0300

    ta_te_ti

--- ta_te_ti.py
+++ ta_te_ti.py
@@ -1,9 +1,9 @@
 tablero = [" " for i in range(9)]
 
 def print_tablero():
-    fila1 = "| {} | {} | {} |".format(tablero[0], tablero[1], tablero[2])
-    fila2 = "| {} | {} | {} |".format(tablero[3], tablero[4], tablero[5])
-    fila3 = "| {} | {} | {} |".format(tablero[6], tablero[7], tablero[8])
+    fila1 = f"| {tablero[0]} | {tablero[1]} | {tablero[2]} |"
+    fila2 = f"| {tablero[3]} | {tablero[4]} | {tablero[5]} |"
+    fila3 = f"| {tablero[6]} | {tablero[7]} | {tablero[8]} |"
 
     print()
     print(fila1)
@@ -16,13 +16,13 @@ def jugador_mov(icono):
         numero = 1
     elif icono == "O":
         numero = 2
-    print("Tu turno jugador {}".format(numero))
-    eleccion = int(input("Ingrese su movimiento (1-9)").strip())
+    print(f"Tu turno jugador {numero}")
+    eleccion = int(input("Ingrese su movimiento (1-9): ").strip())
     if tablero[eleccion - 1] == " ":
         tablero[eleccion - 1] = icono
     else:
         print()
-        print("Ese espacio esta ocupado")
+        print("Ese espacio esta ocupado.")
     
 def es_victoria(icono):
     if (tablero[0] == icono and tablero[1] == icono and tablero[2] == icono) or \
@@ -48,18 +48,18 @@ while True:
     jugador_mov("X")
     print_tablero()
     if es_victoria("X"):
-        print("X es el ganador, felicitaciones")
+        print("X es el ganador, felicitaciones.")
         break
     elif es_empate():
-        print("Esto es empate")
+        print("Esto es empate.")
         break
     jugador_mov("O")
     if es_victoria("O"):
         print_tablero()
-        print("O es el ganador, felicitaciones")
+        print("O es el ganador, felicitaciones.")
         break
     elif es_empate():
-        print("Esto es empate")
+        print("Esto es empate.")
         break
     
 
